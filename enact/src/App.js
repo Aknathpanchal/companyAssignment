3bf4b87da444af35283f6d3a6a233ef1d4be81e3
Aknathpanchal <100464802+Aknathpanchal@users.noreply.github.com>
2023-06-29 00:01:30 +0530

    search implemented

---
+++ enact/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MediaSearch from './Search';
+
+function App() {
+  return (
+    <div className="App">
+  <MediaSearch/>
+    </div>
+  );
+}
+
+export default App;
